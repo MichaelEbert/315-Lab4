a0bb1de98a9b07abb21e1bb4e2b7ece59ac56e1a
Myron Zhao <mjzhao@csclnx03.ad.calpoly.edu>
2015-03-04 16:19:31 -0800

    line stuff (incomplete)

---
+++ linefigure.asm
@@ -0,0 +1,95 @@
+# sp should be initialized to 8191
+
+# a0 = x0
+# a1 = y0
+# a2 = x1
+# a3 = y1
+
+	addi	$a0, $0, 60
+	addi	$a1, $0, 75
+	addi	$a2, $0, 40
+	addi	$a3, $0, 50
+
+line: 	sub	$s0, $a3, $a1	# $s0 holds y1 - y0
+	sub	$s1, $a2, $a0	# $s1 holds x1 - x0
+	
+
+	###### Computing abs value of y1 - y0 ######
+	add	$t0, $s0, $0	# copies $s0 into $t0
+	slt	$t1, $s0, $0	# is $s0 < 0?
+	beq	$t1, $0, ypos	# branch if positive
+	sub	$t0, $0, $s0	# 0 - $s0 if negative	
+ypos:	add	$s0, $t0, $0	# abs($s0) computed
+
+
+	###### Computing abs value of x1 - x0 ######
+	add	$t0, $s1, $0	# copies $s1 into $t0
+	slt	$t1, $s1, $0	# is $s1 < 0?
+	beq	$t1, $0, xpos	# branch if positive
+	sub	$t0, $0, $s1	# 0 - $s1 if negative
+xpos:	add	$s1, $t0, $0	# abs($s1) computed
+
+
+	###### if abs(y1 - y0) > abs(x1 - x0)  ######
+	slt	$t0, $s1, $s0	# is $s1 < $s0
+	add	$s2, $0, $0	# $s2 is st, $s2 = 0
+	beq	$t0, $0, else0	# branch if else case
+	addi	$s2, $0, 1	# $s2 = 1 if we fall through
+
+
+	###### if st == 1 and swapping ######
+else0:	addi	$t0, $0, 1	# $t0 = 1
+	bne	$s2, $t0, else1	# do not swap
+	add	$t0, $a0, $0	# $t0 = $a0
+	add	$a0, $a1, $0	# $a0 = $a1
+	add	$a1, $t0, $0	# $a1 = $t0
+	add	$t0, $a2, $0	# $t0 = $a2
+	add	$a2, $a3, $0	# $a2 = $a3
+	add	$a3, $t0, $0	# $a3 = $t0
+
+
+	###### if x0 > x1 and swapping #######
+else1:	slt	$t0, $a2, $a0	# is x0 > x1?
+	beq	$t0, $0, else2	# do not swap
+	add	$t0, $a0, $0	# $t0 = $a0
+	add	$a0, $a2, $0	# $a0 = $a2
+	add	$a2, $t0, $0	# $a2 = $t0
+	add	$t0, $a1, $0	# $t0 = $a1
+	add	$a1, $a3, $0	# $a1 = $a3
+	add	$a3, $t0, $0	# $a3 = $t0
+
+else2:	sub	$s0, $a2, $a0	# $s0 = deltax = x1 - x0
+	sub	$s1, $a3, $a1	# $s1 = y1 - y0
+
+	
+	###### Computing abs value for deltay ######
+	add	$t0, $s1, $0	# copies $s1 into $t0
+	slt	$t1, $s1, $0	# is $s1 < 0?
+	beq	$t1, $0, ypos2	# branch if positive
+	sub	$t0, $0, $s1	# 0 - $s0 if negative
+ypos2:	add	$s1, $t0, $0	# abs($s1) computed
+
+
+	add	$s3, $0, $0	# $s3 = error = 0
+	add	$s4, $a1, $0	# $s4 = y = y0
+
+	
+	###### if y0 < y1 ######
+	slt	$t0, $a1, $a3	# is $a1 < $a3 (y0 < y1)
+	addi	$s5, $0, 1	# $s5 = ystep = 1
+	bne	$t0, $0, else3	# branch if y0 < y1 (skip else)
+	addi	$s5, $0, -1	# $s5 = ystep = -1
+
+
+	###### For loop ######
+else3:	add	$s6, $a0, $0	# $s6 = x = x0
+	add	$t7, $$a2, $0	# $t7 = x1 + 1
+
+for1:	beq	$s6, $t7, end4	# end for loop if equal (x0 to x1 inclusive)
+
+	addi	$t0, $s0, 1	# $t0 = 1
+	bne	$s2, $t0, else4	# if $s2 ==1 (if st == 1) 
+	sw	$s4, 0($s7)	# plotting y
+	addi	$s7, $s7, 4	# incrementing data mem loc
+	sw	$s6, 0($s7)	# plotting x
+	addi	$s7, $s7, 4	# incrementing data mem loc
