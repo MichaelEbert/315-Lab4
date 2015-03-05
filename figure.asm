29b8745c401aeb575700dfa3df62283d5002917c
Myron Zhao <mjzhao@csclnx03.ad.calpoly.edu>
2015-03-04 20:00:37 -0800

    lab 5 finished

--- figure.asm
+++ figure.asm
@@ -5,7 +5,7 @@ addi	$sp, $0, 8191
 addi	$a0, $0, 30	#head
 addi	$a1, $0, 100
 addi	$a2, $0, 20
-#jal	circle
+jal	circle
 
 addi	$a0, $0, 30	#body
 addi	$a1, $0, 80
@@ -40,12 +40,12 @@ jal	line
 addi	$a0, $0, 24	#left eye
 addi	$a1, $0, 105
 addi	$a2, $0, 3
-#jal	circle
+jal	circle
 
 addi	$a0, $0, 36	#right eye
 addi	$a1, $0, 105
 addi	$a2, $0, 3
-#jal	circle
+jal	circle
 
 addi	$a0, $0, 25	#mouth center
 addi	$a1, $0, 90
@@ -80,14 +80,14 @@ j	end
 #$a1 = yc
 #$a2 = r
 
-#$s0 = xc#
-#$s1 = yc#
-#$s2 = x#
-#$s3 = y#
-#$s4 = g#
-#$s5 = diagonalInc#
-#$s6 = rightInc#
-#$t0 = temp#
+#$s0 = xc
+#$s1 = yc
+#$s2 = x
+#$s3 = y
+#$s4 = g
+#$s5 = diagonalInc
+#$s6 = rightInc
+#$t0 = temp
 
 #$s7 = next address to plot to
 
@@ -105,7 +105,8 @@ circle:
 	sw $s6, 24($sp)
 	sw $ra, 28($sp)
 	#initialize our registers
-	addi $s2, $s2, 0
+	#x = 0, y = r
+	addi $s2, $0, 0 ##line 10
 	addi $s3, $a2, 0
 	#g = 3 - 2*r
 	add $s4, $a2, $a2
@@ -116,26 +117,27 @@ circle:
 	sub $s4, $t0, $s4
 	#diagonalInc
 	addi $t0, $0, 10
-	sub $t3, $t0, $t3
+	sub $s5, $t0, $s5
 	#rightInc
 	addi $s6, $0, 6
 	#xc and yc
 	addi $s0, $a0, 0
-	addi $s1, $a1, 0
+	addi $s1, $a1, 0 ##line 20
 
-#x <= y === y >= x
+#x Lessthan or EQualto y, which is the same as !(x>y)
 xleqy:	slt $t0, $s3, $s2
 	bne $t0, $0, xend
-
-	add $a0, $a0, $s2
-	add $a1, $a1, $s3
+	
+    #the plots
+	add $a0, $s0, $s2
+	add $a1, $s1, $s3
 	jal plot
 	sub $a1, $s1, $s3
 	jal plot
 
 	sub $a0, $s0, $s2
 	add $a1, $s1, $s3
-	jal plot
+	jal plot ##line 30
 	sub $a1, $s1, $s3
 	jal plot
 	
@@ -147,28 +149,30 @@ xleqy:	slt $t0, $s3, $s2
 
 	sub $a0, $s0, $s3
 	add $a1, $s1, $s2
-	jal plot
+	jal plot ##line 40
 	sub $a1, $s1, $s2
 	jal plot
 
-	#if g >= 0: g+= diagonalInc
+	##if g >= 0## (g not < 0)
 	slt $t0, $s4, $0
 	bne $t0, $0, gltz
-	add $s4, $s4, $s5 
-	#diagonalInc+=8
+	##g+= diagonalInc##
+	add $s4, $s4, $s5 ##line 45
+	##diagonalInc+=8##
 	addi $s5, $s5, 8
-	#y-=1
+	##y-=1##
 	addi $s3, $s3, -1
 	j gend
-#else
+    ##else##
 gltz:	add $s4, $s4, $s6
-	addi $s5, $s5, 4
+	addi $s5, $s5, 4##line 50
 
-#end if(g>=0) block
+#end of if(g>=0) block
 gend:	addi $s6, $s6, 4
 	addi $s2, $s2, 1
+	j xleqy
 
-#end x<=y loop
+#end of x<=y loop
 xend:
 #end: restore registers
 	lw $s0, 0($sp)
@@ -187,6 +191,16 @@ plot:
 	sw $a1, 4($s7)
 	addi $s7, $s7, 8
 	jr $ra
+	
+main:	addi $s7, $0, 0
+    addi $sp, $0, 8192
+
+	addi $a0, $0, 30
+	addi $a1, $0, 100
+	addi $a2, $0, 20
+	jal circle
+	#need fake instruction to correctly run
+	sll $0, $0, 0
 	
 
 
